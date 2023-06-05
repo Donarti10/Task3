abb1d2051ce204cd30f282a90cf2044b111069e5
Donarti10 <123815608+Donarti10@users.noreply.github.com>
2023-06-05 21:32:53 +0200

    third-commit

--- src/Components/page.js
+++ src/Components/page.js
@@ -12,18 +12,38 @@ function Task( {closeTask} ) {
             </div>
             <div className="body">
             <p>Font Color </p>
+            <div className="box">
+                <p>#4444444</p>
+                <button className="colors"></button>
+            </div>
             </div>
             <div className="p-1">
             <p>Background Color</p>
+            <div className="box">
+                <p>#FFFFFF</p>
+                <button className="colors1"></button>
+            </div>
             </div>
             <div className="p-2">
             <p>Button Color</p>
+            <div className="box">
+                <p>#2072EF</p>
+                <button className="colors2"></button>
+            </div>
             </div>
             <div className="p-3">
             <p>Button Border Color</p>
+            <div className="box">
+                <p>#2072EF</p>
+                <button className="colors3"></button>
+            </div>
             </div>
             <div className="p-4">
             <p>Button Mouseover Color</p>
+            <div className="box">
+                <p>#0053D1</p>
+                <button className="colors4"></button>
+            </div>
             </div>
             
             <div className="btn1">
